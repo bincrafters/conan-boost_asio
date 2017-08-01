9213b9391bd851c9b9a39446a575febbea60b5d5
Jerry W <jerrywiltse@gmail.com>
2017-08-01 14:27:01 -0400

    updated requires

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,30 @@ class BoostAsioConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/asio"
-    description = "For a description of this library, please visit http://boost.org/asio "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "asio"
+    requires =  "Boost.Array/1.64.0@bincrafters/testing", \
+                      "Boost.Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Bind/1.64.0@bincrafters/testing", \
+                      "Boost.Chrono/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Coroutine/1.64.0@bincrafters/testing", \
+                      "Boost.Date_Time/1.64.0@bincrafters/testing", \
+                      "Boost.Function/1.64.0@bincrafters/testing", \
+                      "Boost.Regex/1.64.0@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
+                      "Boost.System/1.64.0@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing"
 
+                      #array3 assert1 bind3 chrono8 config0 core2 coroutine13 date_time11 function5 regex6 smart_ptr4 system3 throw_exception2 type_traits3
+                      
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)
