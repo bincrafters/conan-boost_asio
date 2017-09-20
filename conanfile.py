07501e36eb2a8f6093a90d2c08146676872c1437
Jerry W <jerrywiltse@gmail.com>
2017-09-19 23:30:14 -0400

    changed refs to testing

--- conanfile.py
+++ conanfile.py
@@ -8,20 +8,20 @@ class BoostAsioConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["asio"]
-    requires =  "Boost.Array/1.65.1@bincrafters/stable", \
-                      "Boost.Assert/1.65.1@bincrafters/stable", \
-                      "Boost.Bind/1.65.1@bincrafters/stable", \
-                      "Boost.Chrono/1.65.1@bincrafters/stable", \
-                      "Boost.Config/1.65.1@bincrafters/stable", \
-                      "Boost.Core/1.65.1@bincrafters/stable", \
-                      "Boost.Coroutine/1.65.1@bincrafters/stable", \
-                      "Boost.Date_Time/1.65.1@bincrafters/stable", \
-                      "Boost.Function/1.65.1@bincrafters/stable", \
-                      "Boost.Regex/1.65.1@bincrafters/stable", \
-                      "Boost.Smart_Ptr/1.65.1@bincrafters/stable", \
-                      "Boost.System/1.65.1@bincrafters/stable", \
-                      "Boost.Throw_Exception/1.65.1@bincrafters/stable", \
-                      "Boost.Type_Traits/1.65.1@bincrafters/stable"
+    requires =  "Boost.Array/1.65.1@bincrafters/testing", \
+                      "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Bind/1.65.1@bincrafters/testing", \
+                      "Boost.Chrono/1.65.1@bincrafters/testing", \
+                      "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Coroutine/1.65.1@bincrafters/testing", \
+                      "Boost.Date_Time/1.65.1@bincrafters/testing", \
+                      "Boost.Function/1.65.1@bincrafters/testing", \
+                      "Boost.Regex/1.65.1@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
+                      "Boost.System/1.65.1@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing"
 
                       #array3 assert1 bind3 chrono8 config0 core2 coroutine13 date_time11 function5 regex6 smart_ptr4 system3 throw_exception2 type_traits3
                       
