5db7b4f5e7440344c6541ae49588960b38ab25b9
Jerry W <jerrywiltse@gmail.com>
2018-02-04 00:01:21 -0500

    change ver to 1.65.1 after merge

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans import ConanFile, tools
 
 class BoostAsioConan(ConanFile):
     name = "boost_asio"
-    version = "1.66.0"
+    version = "1.65.1"
     url = "https://github.com/bincrafters/conan-boost_asio"
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
@@ -17,21 +17,21 @@ class BoostAsioConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing",
-        "boost_array/1.66.0@bincrafters/testing",
-        "boost_assert/1.66.0@bincrafters/testing",
-        "boost_bind/1.66.0@bincrafters/testing",
-        "boost_chrono/1.66.0@bincrafters/testing",
-        "boost_config/1.66.0@bincrafters/testing",
-        "boost_core/1.66.0@bincrafters/testing",
-        "boost_coroutine/1.66.0@bincrafters/testing",
-        "boost_date_time/1.66.0@bincrafters/testing",
-        "boost_function/1.66.0@bincrafters/testing",
-        "boost_regex/1.66.0@bincrafters/testing",
-        "boost_smart_ptr/1.66.0@bincrafters/testing",
-        "boost_system/1.66.0@bincrafters/testing",
-        "boost_throw_exception/1.66.0@bincrafters/testing",
-        "boost_type_traits/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/testing",
+        "boost_array/1.65.1@bincrafters/testing",
+        "boost_assert/1.65.1@bincrafters/testing",
+        "boost_bind/1.65.1@bincrafters/testing",
+        "boost_chrono/1.65.1@bincrafters/testing",
+        "boost_config/1.65.1@bincrafters/testing",
+        "boost_core/1.65.1@bincrafters/testing",
+        "boost_coroutine/1.65.1@bincrafters/testing",
+        "boost_date_time/1.65.1@bincrafters/testing",
+        "boost_function/1.65.1@bincrafters/testing",
+        "boost_regex/1.65.1@bincrafters/testing",
+        "boost_smart_ptr/1.65.1@bincrafters/testing",
+        "boost_system/1.65.1@bincrafters/testing",
+        "boost_throw_exception/1.65.1@bincrafters/testing",
+        "boost_type_traits/1.65.1@bincrafters/testing"
     )
         
     # BEGIN
@@ -39,7 +39,7 @@ class BoostAsioConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
