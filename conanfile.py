51d10751bc347d7399ef686471c6b15b60cbae7a
Jerry W <jerrywiltse@gmail.com>
2018-02-17 09:39:39 -0500

    merge from testing

--- conanfile.py
+++ conanfile.py
@@ -17,21 +17,21 @@ class BoostAsioConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.65.1@bincrafters/testing",
-        "boost_array/1.65.1@bincrafters/testing",
-        "boost_assert/1.65.1@bincrafters/testing",
-        "boost_bind/1.65.1@bincrafters/testing",
-        "boost_chrono/1.65.1@bincrafters/testing",
-        "boost_config/1.65.1@bincrafters/testing",
-        "boost_core/1.65.1@bincrafters/testing",
-        "boost_coroutine/1.65.1@bincrafters/testing",
-        "boost_date_time/1.65.1@bincrafters/testing",
-        "boost_function/1.65.1@bincrafters/testing",
-        "boost_regex/1.65.1@bincrafters/testing",
-        "boost_smart_ptr/1.65.1@bincrafters/testing",
-        "boost_system/1.65.1@bincrafters/testing",
-        "boost_throw_exception/1.65.1@bincrafters/testing",
-        "boost_type_traits/1.65.1@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/stable",
+        "boost_array/1.65.1@bincrafters/stable",
+        "boost_assert/1.65.1@bincrafters/stable",
+        "boost_bind/1.65.1@bincrafters/stable",
+        "boost_chrono/1.65.1@bincrafters/stable",
+        "boost_config/1.65.1@bincrafters/stable",
+        "boost_core/1.65.1@bincrafters/stable",
+        "boost_coroutine/1.65.1@bincrafters/stable",
+        "boost_date_time/1.65.1@bincrafters/stable",
+        "boost_function/1.65.1@bincrafters/stable",
+        "boost_regex/1.65.1@bincrafters/stable",
+        "boost_smart_ptr/1.65.1@bincrafters/stable",
+        "boost_system/1.65.1@bincrafters/stable",
+        "boost_throw_exception/1.65.1@bincrafters/stable",
+        "boost_type_traits/1.65.1@bincrafters/stable"
     )
         
     # BEGIN
@@ -39,7 +39,7 @@ class BoostAsioConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.65.1@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/stable"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
