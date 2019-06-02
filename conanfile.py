e112925fce9e3e45bccfd2c3cc9642120e81f09a
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:23 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,28 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostAsioConan(base.BoostBaseConan):
     name = "boost_asio"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_asio"
-    lib_short_names = ["asio"]
-    header_only_libs = ["asio"]
-    b2_requires = [
-        "boost_array",
-        "boost_assert",
-        "boost_bind",
-        "boost_chrono",
-        "boost_config",
-        "boost_core",
-        "boost_coroutine",
-        "boost_date_time",
-        "boost_function",
-        "boost_regex",
-        "boost_smart_ptr",
-        "boost_system",
-        "boost_throw_exception",
-        "boost_type_traits",
-        "boost_utility"
-    ]
+    version = "1.70.0"
